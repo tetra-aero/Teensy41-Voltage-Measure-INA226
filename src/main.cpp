3e8d9e52eb5857c21a483d6badb6d62f2922c22f
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2022-10-19 16:16:15 +0900

    add led on/off
    add led on/off

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+//#include <Arduino.h>
+#include "LEDblink.hpp"
+
+#if defined(ESP32)
+// for ESP32 devkit v4
+#define LED_BUILTIN 2
+#endif
+
+LEDblink led_test;
+
+void setup() {
+    led_test.init(LED_BUILTIN);
+    Serial.begin(115200);
+}
+ 
+void loop() {
+    led_test.led_on();
+    Serial.println("LED On!!"); 
+    delay(2000); 
+    led_test.led_off();
+    Serial.println("LED Off!!");
+    delay(2000); 
+}
